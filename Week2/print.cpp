c1ca9f5bf964e5699b8d18fdf1081662f02b5c6f
asegura4488 <asegura4488@gmail.com>
2021-02-02 13:44:07 -0500

    first c++ code

---
+++ Week2/print.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main(){
+
+  // C++ es un lenguaje fuertemente tipado
+
+  int a;
+
+  std::cout << " --- Escriba un numero entero --- " << std::endl; 
+  std::cin >> a;
+
+  float b = 5.1121;
+  double c = 6.28358;
+
+  std::cout << a  << " " << b << std::endl;
+
+  return 0;
+
+}
+
+
